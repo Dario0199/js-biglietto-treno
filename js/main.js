a681ac8b1762f9adafa615b8277eb65221d6f05a
Dario0122 <dario.di.cuia@gmail.com>
2021-10-21 16:37:54 +0200

    prezzo

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//chiedi i chilometri da percorrere
+let chilometri = prompt('inserisci i chilometri che vuoi percorrere');
+
+
+//chiedi età del passeggero
+let age = prompt('inserisci la tua età')
+
+
+//prezzo biglietto
+let price = chilometri * 0.21;
+console.log(price)
+
+
+//prezzo totale
+let totalPrice;
+
+if(age < 18){
+    // console.log('minorenne')
+    totalPrice = price - ((price / 100)* 20)
+} else if (age > 65){
+    // console.log('over 65')
+    totalPrice = price - ((price / 100)* 40)
+} else {
+    // console.log('normal')
+    totalPrice = price
+}
+
+document.getElementById('ticketPrice').innerHTML = totalPrice;
